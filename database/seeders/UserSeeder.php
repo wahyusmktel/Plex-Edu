acb9bab4544d45c1e6e9c0dcc1d35a01a8ee1e88
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-01-13 07:59:15 +0700

    Fix: User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Siswa;
+use App\Models\Kelas;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -31,13 +33,29 @@ class UserSeeder extends Seeder
             'role' => 'guru',
         ]);
 
+        // Kelas Default
+        $kelas = Kelas::create([
+            'nama' => 'X MIPA 1',
+            'tingkat' => '10',
+            'kapasitas' => 36
+        ]);
+
         // Siswa
-        User::create([
+        $userSiswa = User::create([
             'name' => 'Siswa Literasia',
             'email' => 'siswa@literasia.com',
             'username' => 'siswa',
             'password' => Hash::make('password'),
             'role' => 'siswa',
+        ]);
+
+        Siswa::create([
+            'user_id' => $userSiswa->id,
+            'kelas_id' => $kelas->id,
+            'nis' => '12345',
+            'nisn' => '0012345678',
+            'nama_lengkap' => 'Siswa Literasia',
+            'jenis_kelamin' => 'L',
         ]);
     }
 }
