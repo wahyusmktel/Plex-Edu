1e95087933dd7e4136e9bda10121cfb8e4c70ee5
Wahyu Rahmat Hidayat <99480903+wahyusmktel@users.noreply.github.com>
2026-01-30 01:13:49 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Pagination\Paginator::useTailwind();
 
         // Share application settings across all views
